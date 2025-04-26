84c67947a16a763b1dc5c1817529bbb9e97d8ea7
hridoy ahmed <sahmed221068@bscse.uiu.ac.bd>
2025-04-26 23:58:57 +0600

    first file

---
+++ codeforce.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename S, typename T> void smax(S &a, T b) { if (a < b) a = b; }
+template <typename S, typename T> void smin(S &a, T b) { if (a > b) a = b; }
+template <typename T> void swap(T &a, T &b) { T t = a; a = b; b = t; }
+#define int long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size() // what it does is return the size of the vector or string x 
+#define loop(i, a, b) for (int i = a; i < b; i++)
+const int mxn = 1e5 + 5;
+const int inf = 1e18;
+signed main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+   
+}
